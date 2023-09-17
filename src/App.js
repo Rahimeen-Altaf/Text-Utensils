2d6319c73405ba168b6b12abbd6a948beaecbb62
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2023-09-17 05:26:08 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <div className="container">
+        <h1>Rahi</h1>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores illo repudiandae delectus.</p>
+      </div>
+    </nav>
+    </>
   );
 }
 
