4779c55a5d3a8b97b729286437d4a1d8c3228aa4
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2023-09-17 07:59:35 +0500

    added new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <div className="container">
-        <h1>Rahi</h1>
-
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores illo repudiandae delectus.</p>
-      </div>
-    </nav>
+    <Navbar title="TextUtils"/>
+    <div className="container my-3">
+      <TextForm heading="Enter the text to analyze below"/>
+    </div>
     </>
   );
 }
