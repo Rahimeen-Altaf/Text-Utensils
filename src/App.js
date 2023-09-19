bd7893c880030c77454a88dd0f0407c0d6c0af92
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2023-09-19 05:48:58 +0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   
@@ -45,12 +46,16 @@ function App() {
 
   return (
     <>
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-      {/* <About/> */}
+      <Routes>
+        <Route path="/about" element={<About />}></Route>	
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}></Route>	
+      </Routes>
     </div>
+    </Router>
     </>
   );
 }
