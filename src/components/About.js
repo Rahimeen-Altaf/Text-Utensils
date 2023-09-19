c26e81b979f4b5df00030b356c60feb7c9585d58
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2023-09-19 11:07:22 +0500

    Modified the About Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,75 +1,56 @@
-import React, { useState } from 'react'
+export default function About(props) {
 
-export default function About() {
-
-
-    const [myStyle, setMyStyle] = useState({
-            color: 'black',
-            backgroundColor: 'white'
-    })
-
-    const [btnText, setbtnText] = useState("Enable Dark Mode");
-
-    const toggleStyle = () => {
-        if(myStyle.color === "black") {
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white'
-        });
-        setbtnText("Enable Light Mode");
-        } else {
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white'
-        });  
-        setbtnText("Enable Dark Mode");
-        }
+    document.title = props.title;
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#042743',
+        backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
     }
 
   return (
-    <div className='container' style={myStyle}>
+    <div className='container' style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
         <h1 className="my-3">About Us</h1>
             <div className="accordion" id="accordionExample" style={myStyle}>
         <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
+            <strong>About TextXpress</strong>
             </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                The <strong>TextXpress</strong> is a versatile web application that allows users to perform various operations on text. Whether you need to format text, analyze its properties, or even have it read aloud, this tool has you covered. Developed using <code>React.js</code>, this project provides a user-friendly interface for text processing.
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accordion Item #2
+               <strong>Usage</strong>
             </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <ul style={{ listStyleType: "decimal" }}>
+                <li>Enter or paste text into the input area.</li>
+                <li>Choose from a variety of text manipulation options.</li>
+                <li>View the modified text, word count, and reading time in real-time.</li>
+                <li>Customize your experience with the built-in dark mode feature.</li>
+                </ul>
             </div>
             </div>
         </div>
         <div className="accordion-item">
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Accordion Item #3
+            <strong>Motivation</strong>
             </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>TextXpress</strong> was build to provide a convenient and accessible tool for users to manipulate text in a variety of ways. Whether you're  a writer, student, or anyone dealing with text regularly, this tool can help streamline your tasks while offering a pleasant <code>dark mode</code> for those late-night sessions.
             </div>
             </div>
         </div>
-        </div>
-        <div className="container my-3">
-        <button type='button' onClick={toggleStyle} className='btn btn-primary'>{btnText}</button> 
         </div>
     </div>
   )
